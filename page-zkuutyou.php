2bbc27c3e9528bcd7add520f1eb6934c609f8691
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-07-02 15:17:09 +0900

    Z空調ページ追加

---
+++ page-zkuutyou.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Template Name: Z空調の家ページテンプレート
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<img src="<?php echo get_template_directory_uri(); ?>/images/top-img2.jpg" alt="トップ画像" width="100%">
+
+<div class="container-fluid bg-lightbrown">
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+            <p class="d-inline-block bg-white z-title">Z空調の家<span class="text-brown">とは?</span></p>
+            <p class="text-center text-darkbrown pb40 z-text">一年中、家の温度を快適に。<br>Z空調は、身体にも家計にもやさしい、新時代冷暖システムです。</p>
+            <img class="z-crosssection-img" src="<?php echo get_template_directory_uri(); ?>/images/z-crosssection.png" alt="断面図" width="100%">
+        </div>
+    </div>
+</div>
+</div>
+
+<?php get_footer(); ?>
