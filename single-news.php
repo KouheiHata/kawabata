f311e07944b33bcfe4648d37e1280d100b19b3e9
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2020-07-21 15:42:20 +0900

    当日予約の注意書き

--- single-news.php
+++ single-news.php
@@ -29,11 +29,14 @@ Template Name: 川端工務店固定ページ
             </div>
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-3">
-			<div style="padding: 10px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px; line-height: 2;">
+			<div style="padding: 10px; margin-bottom: 10px; border: 1px dashed #333333; border-radius: 5px; line-height: 1.5;">
 			【新型コロナウイルスに関する対応について】<br />
 			■スタッフの検温やアルコール消毒を徹底し、マスク着用で接客をさせていただきます。<br />
 			■ご来場いただいたお客様には、アルコール消毒及び、手袋・マスクの着用をお願い致します。<br />
+			<br />
 			ご来場いただきます皆様の安全を配慮し、室内の定期的な換気などを行いながら見学会を開催させていただきますので、ご理解・ご協力の程よろしくお願いいたします。<br />
+			<br />
+			※当日の予約は電話でお願いします。
 			</div>
 		</div>
     </div>
