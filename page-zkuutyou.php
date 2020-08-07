27f68c2e99bcbdc7ba0ebb5647a03e786c80f166
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2020-08-07 14:36:19 +0900

    ボタンの微調整
    色の変更。マウスカーソルの変更。

--- page-zkuutyou.php
+++ page-zkuutyou.php
@@ -74,12 +74,12 @@ Template Name: Z空調の家ページテンプレート
             <div>
                 <div class="text-center btn-box">
                     <div>
-                        <a href="<?php echo home_url('/contact'); ?>"><button class="text-center url z-btn">Z空調についての<br>お問い合わせ</button></a>
+                        <a href="<?php echo home_url('/contact'); ?>"><button class="text-center url z-btn z-bg">Z空調についての<br>お問い合わせ</button></a>
                     </div>
                 </div>
                 <div class="text-center">
                     <div>
-                        <a href="https://www.hinokiya.jp/z/" class="url z-btn" target="_blank" rel="noopener">詳しくはこちら</a>
+                        <a href="https://www.hinokiya.jp/z/" class="url z-btn z-bg2" target="_blank" rel="noopener">詳しくはこちら</a>
                         <p class="text-center pt30">※別サイトにジャンプします</p>
                     </div>
                 </div>
