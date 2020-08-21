214898ee6a0b3de725a7973798a196a15bae8d91
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-08-21 15:11:19 +0900

    採用情報ページ作成

---
+++ page-adoption.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+Template Name: 採用情報ページテンプレート
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 adop-top">
+            <div class="adop-titlebox">
+                <h3>《戸建住宅の現場監督》福井近隣エリアにおける現場の監督業務</h3>
+                <p class="d-inline-block">正社員</p>
+                <img>
+            </div>
+            <div class="adop-textbox">
+                <h4>弊社と共に、現場の指揮・監督の担当者として、ご活躍いただける方を募集します。</h4>
+                <p>当社は、建材から木材まで、幅広い建築資材の取り扱いのある会社や、各種協力業者様との連携で、分譲住宅を中心に、福井県内で建築/販売実績のある工務店です。<br>新たな人員の導入に際し、現場の指揮・監督の担当者として、ご活躍いただける方を募集します。</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 table-box">
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th scope="row">職種/仕事内容</th>
+                        <td>テストテストテスト</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">雇用形態</th>
+                        <td>テストテストテスト</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">募集対象</th>
+                        <td>テストテストテスト</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">給与</th>
+                        <td>テストテストテスト</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">諸手当</th>
+                        <td>テストテストテスト</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">昇給</th>
+                        <td>テストテストテスト</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
