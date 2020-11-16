943868893113659e2136731ebe37c012eb0697c8
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-11-16 15:02:11 +0900

    長期優良住宅ページ作成開始

---
+++ page-longterm.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Template Name: 長期優良住宅ページテンプレート
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container-fluid my-5">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-1.png">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <p class="pt-3">長く安心して住むことができる、国の技術基準をみたした住居「長期優良住宅」。<br>川端工務店は、品質が保証された本当に良い家で、お客様の暮らしを支えます。</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
