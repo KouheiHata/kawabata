8fbcbae62b091cac22cc9c25f640b6e40b3a0567
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-11-17 15:30:23 +0900

    長期優良住宅ページ作業

--- page-longterm.php
+++ page-longterm.php
@@ -7,12 +7,57 @@ Template Name: 長期優良住宅ページテンプレート
 
 <?php get_header(); ?>
 
-<div class="container-fluid my-5">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-1.png">
+<div class="container-fluid p-0 my-5 lh-box">
+    <div class="bg-lhtop">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 lh-title">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-1.png" alt="タイトル" width="700px">
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt-4">
+                <p class="lh-top-text">長く安心して住むことができる、国の技術基準をみたした住居「長期優良住宅」。<br>川端工務店は、品質が保証された本当に良い家で、お客様の暮らしを支えます。</p>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 pt8">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-2.png" alt="見出し" width="220px">
+                <p class="lh-sub-text pt2">長期優良住宅とは、長い間安心して心地よく暮らせるよう<br>国の定めた技術基準をみたした認定住宅です。<br>認定を受けた住宅はメリットもたくさん。<br>補助金、住宅ローンの金利引き下げ、税の特例や<br>地震保険料の割引などを受けることもできます。</p>
+                <div class="box-bottom"><img src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-4.png" alt="図" width="500px"></div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 pt8">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/LH/k1@2x.png" alt="イメージ画像" width="600px">
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt-5">
+                <div>
+                    <h2>維持保全計画</h2>
+                    <p>建築時から将来を見据えて、定期的な点検・補修などに関する計画が策定されていること。</p>
+                </div>
+                <div class="pt-3">
+                    <h2>住戸面積</h2>
+                    <p>良好な居住水準を確保するために必要な規模を有すること。<br>住戸の少なくとも1の階の床面積（階段部分の面積を除く）が40㎡以上であること。</p>
+                </div>
+                <div class="pt-3">
+                    <h2>居住環境</h2>
+                    <p>良好な景観の形成、そのほかの地域における居住環境の維持及び向上に配置されたものであること。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid p-0 mb-5 lh-box">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-5.png" alt="タイトル">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt-4">
+                <h2 class="text-center">その他長期優良住宅の特徴</h2>
+            </div>
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <p class="pt-3">長く安心して住むことができる、国の技術基準をみたした住居「長期優良住宅」。<br>川端工務店は、品質が保証された本当に良い家で、お客様の暮らしを支えます。</p>
+                <img class="d-block" src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-6.png" alt="タイトル" width="335px">
+                <img class="d-block pt-4 pl55" src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-7.png" alt="表" width="680px">
             </div>
         </div>
     </div>
