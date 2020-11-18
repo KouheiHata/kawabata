422ec997e3c2236af2f1da1e76961fe64c400fb0
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-11-18 15:03:27 +0900

    長期優良住宅ページ作業

--- page-longterm.php
+++ page-longterm.php
@@ -32,15 +32,15 @@ Template Name: 長期優良住宅ページテンプレート
             </div>
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt-5">
                 <div>
-                    <h2>維持保全計画</h2>
+                    <h2 class="text-lb">維持保全計画</h2>
                     <p>建築時から将来を見据えて、定期的な点検・補修などに関する計画が策定されていること。</p>
                 </div>
                 <div class="pt-3">
-                    <h2>住戸面積</h2>
+                    <h2 class="text-lb">住戸面積</h2>
                     <p>良好な居住水準を確保するために必要な規模を有すること。<br>住戸の少なくとも1の階の床面積（階段部分の面積を除く）が40㎡以上であること。</p>
                 </div>
                 <div class="pt-3">
-                    <h2>居住環境</h2>
+                    <h2 class="text-lb">居住環境</h2>
                     <p>良好な景観の形成、そのほかの地域における居住環境の維持及び向上に配置されたものであること。</p>
                 </div>
             </div>
@@ -48,16 +48,59 @@ Template Name: 長期優良住宅ページテンプレート
     </div>
 </div>
 
-<div class="container-fluid p-0 mb-5 lh-box">
+<div class="container-fluid p-0 lh-box">
     <img src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-5.png" alt="タイトル">
-    <div class="container">
+    <div class="container mb-5">
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 pt-4">
-                <h2 class="text-center">その他長期優良住宅の特徴</h2>
+                <h2 class="text-center text-lb">その他長期優良住宅の特徴</h2>
             </div>
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <img class="d-block" src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-6.png" alt="タイトル" width="335px">
-                <img class="d-block pt-4 pl55" src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-7.png" alt="表" width="680px">
+                <img class="d-block" src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-6.png" alt="吹き出し" width="156px">
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/k4@2x.png" alt="吹き出し" width="49px">
+                    <h2 class="d-inline">住宅ローン減税の拡充</h2>
+                </div>
+                <div class="pl55">
+                    <p class="pt-4"><span class="ls53">居住年</span>：平成26年（4月）から 令和3年（12月末まで）<br>控除期間：10年間<br><span class="ls53">控除率</span>：1.0%</p>
+                    <img class="d-block pt-3" src="<?php echo get_template_directory_uri(); ?>/images/LH/lh-7.png" alt="表" width="680px">
+                </div>
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/k4@2x.png" alt="吹き出し" width="49px">
+                    <h2 class="d-inline">投資減税型の特別控除</h2>
+                </div>
+                <div class="pl55">
+                    <p>（平成 21 年 6 月 4 日から令和 3 年 12 月 31 日までに入居した者が対象）<br>個人が、長期優良住宅の普及の促進に関する法律に規定する認定長期優良住宅の新築等をして、<br>居住の用に供した場合には、標準的な性能強化費用相当額 の 10 ％相当額を、<br>その年分の所得税額から控除（当該控除をしてもなお控除しきれない金額がある場合には、<br>翌年分の所得税額から控除）する。</p>
+                </div>
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/k4@2x.png" alt="吹き出し" width="49px">
+                    <h2 class="d-inline">登録免許税</h2>
+                </div>
+                <div class="pl55">
+                    <p>（令和４年３月 31 日までに取得した者が対象）<br>住宅用家屋の所有権保存登記等に係る税率を一般住宅特例より引き下げる。</p>
+                </div>
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/k4@2x.png" alt="吹き出し" width="49px">
+                    <h2 class="d-inline">不動産取得税</h2>
+                </div>
+                <div class="pl55">
+                    <p>（令和４ 年３月 31 日までに新築された住宅が対象）<br>新築住宅に係る不動産取得税について、課税標準からの控除額を一般住宅特例より増額する。</p>
+                </div>
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/LH/k4@2x.png" alt="吹き出し" width="49px">
+                    <h2 class="d-inline">固定資産税</h2>
+                </div>
+                <div class="pl55">
+                    <p>（令和４ 年３月 31 日 までに新築された住宅が対象）<br>新築住宅に係る固定資産税の減額措置の適用期間を一般住宅より延長する。</p>
+                </div>
             </div>
         </div>
     </div>
