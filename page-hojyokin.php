a6d9b42b857f12000c0ed0fa78b8154699916734
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-12-10 14:02:50 +0900

    補助金ページ作成

---
+++ page-hojyokin.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+Template Name: 補助金ページ
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container subsidy-box my-5">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/manga.png" alt="漫画">
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
+            <div class="mt250">
+                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img1.png" alt="説明">
+            </div>
+            <div class="mt130">
+                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img2.png" alt="吹き出し">
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
+            <div>
+                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img3.png" alt="図">
+            </div>
+            <div class="mt-5">
+                <p>※1&emsp;2020年4月1日時点で婚姻から3年経過しない夫婦</p>
+                <p>※2&emsp;福井県内他市町から：2020年4月1日時点で越前市に転入して半年を経過していないもの<br>&emsp;&emsp;&ensp;福井県外から：2020年4月1日時点で越前市に転入して3年を経過していないもの</p>
+            </div>
+            <div class="my150">
+                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img4.png" alt="吹き出し">
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
+            <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img5.png" alt="こだわり1" width="196px">
+            <div class="mx-auto mt-5" style="max-width:240px;">
+                <p>経営理念の『信頼』の元、お施主様に合った生活の提案ができます。</p>
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
+            <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img6.png" alt="こだわり2" width="156px">
+            <div class="mx-auto mt-5" style="max-width:240px;">
+                <p>創業35年以上にわたり、住宅に携わってきた豊富な『実績』『技術』を生かして家作りをします。</p>
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
+            <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img7.png" alt="こだわり3" width="181px">
+            <div class="mx-auto mt-4" style="max-width:240px;">
+                <p>新築のお家はもちろんのこと、リフォーム後のお家のメンテナンスやお困り事にも誠意をもって対応します。</p>
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt100">
+            <div>
+                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img8.png" alt="吹き出し">
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 my150">
+            <div class="text-center">
+                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img9.png" alt="全館空調" width="811px">
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="text-center">
+                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/zkuchou_2.jpg" alt="Z空調">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
