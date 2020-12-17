2ec409dffc0e9f0be4259f99ffb5c245992413e8
shi-hatta <s_hatta@standtogether-fukushi.com>
2020-12-17 09:41:42 +0900

    レスポンシブ調整

--- page-hojyokin.php
+++ page-hojyokin.php
@@ -10,59 +10,95 @@ Template Name: 補助金ページ
 <div class="container subsidy-box my-5">
     <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/manga.png" alt="漫画">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/subsidy/manga.png" alt="漫画">
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
-            <div class="mt250">
-                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img1.png" alt="説明">
+            <div class="d-none d-md-block mt250">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img1.png" alt="説明">
             </div>
-            <div class="mt130">
-                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img2.png" alt="吹き出し">
+            <div class="d-block d-md-none pt30">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img10.png" alt="説明">
+            </div>
+            <div class="mt130 text-center">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img2.png" alt="吹き出し">
             </div>
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt-5">
-            <div>
-                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img3.png" alt="図">
+            <div class="d-none d-md-block">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img3.png" alt="図">
+            </div>
+            <div class="d-block d-md-none">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img14.png" alt="図">
             </div>
             <div class="mt-5">
                 <p>※1&emsp;2020年4月1日時点で婚姻から3年経過しない夫婦</p>
-                <p>※2&emsp;福井県内他市町から：2020年4月1日時点で越前市に転入して半年を経過していないもの<br>&emsp;&emsp;&ensp;福井県外から：2020年4月1日時点で越前市に転入して3年を経過していないもの</p>
+                <p>※2&emsp;福井県内他市町から：2020年4月1日時点で越前市<span class="d-inline d-sm-none"><br>&emsp;&emsp;&ensp;</span>に転入して半年を経過していないもの<br>&emsp;&emsp;&ensp;福井県外から：2020年4月1日時点で越前市に転入<span class="d-inline d-sm-none"><br>&emsp;&emsp;&ensp;</span>して3年を経過していないもの</p>
             </div>
-            <div class="my150">
-                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img4.png" alt="吹き出し">
+            <div class="my150 text-center">
+                <img class="d-none d-md-inline" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img4.png" alt="吹き出し">
+                <img class="d-inline d-md-none" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img16.png" alt="吹き出し">
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 d-block d-md-none">
+            <div>
+                <p class="text-center">COMMITMENT</p>
+                <h3 class="text-center">川端工務店のこだわり</h3>
             </div>
         </div>
         <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
-            <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img5.png" alt="こだわり1" width="196px">
-            <div class="mx-auto mt-5" style="max-width:240px;">
+            <img class="d-none d-md-inline" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img5.png" alt="こだわり1" width="196px">
+            <img class="d-inline d-md-none mt50" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img11.png" alt="こだわり1" width="147px">
+            <div class="mx-auto mt-5 d-none d-md-block" style="max-width:240px;">
                 <p>経営理念の『信頼』の元、お施主様に合った生活の提案ができます。</p>
             </div>
-        </div>
-        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
-            <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img6.png" alt="こだわり2" width="156px">
-            <div class="mx-auto mt-5" style="max-width:240px;">
-                <p>創業35年以上にわたり、住宅に携わってきた豊富な『実績』『技術』を生かして家作りをします。</p>
+            <div class="mx-auto mt-5 d-block d-md-none sm-text-block" style="max-width:188px;">
+                <h4 class="text-center">こだわりその①</h4>
+                <p class="text-center">経営理念の『信頼』の元、<br>お施主様に合った生活の提案ができます。</p>
             </div>
         </div>
         <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
-            <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img7.png" alt="こだわり3" width="181px">
-            <div class="mx-auto mt-4" style="max-width:240px;">
+            <img class="d-none d-md-inline" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img6.png" alt="こだわり2" width="156px">
+            <img class="d-inline d-md-none mt50" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img12.png" alt="こだわり2" width="107px">
+            <div class="mx-auto mt-5 d-none d-md-block" style="max-width:240px;">
+                <p>創業35年以上にわたり、住宅に携わってきた豊富な『実績』『技術』を生かして家作りをします。</p>
+            </div>
+            <div class="mx-auto mt-5 d-block d-md-none sm-text-block" style="max-width:188px;">
+                <h4 class="text-center">こだわりその②</h4>
+                <p class="text-center">創業35年以上にわたり、住宅に携わってきた豊富な『実績』『技術』を生かして家作りをします。</p>
+            </div>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
+            <img class="d-none d-md-inline" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img7.png" alt="こだわり3" width="181px">
+            <img class="d-inline d-md-none mt50" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img13.png" alt="こだわり3" width="104px">
+            <div class="mx-auto mt-4 d-none d-md-block" style="max-width:240px;">
                 <p>新築のお家はもちろんのこと、リフォーム後のお家のメンテナンスやお困り事にも誠意をもって対応します。</p>
+            </div>
+            <div class="mx-auto mt-5 d-block d-md-none sm-text-block" style="max-width:188px;">
+                <h4 class="text-center">こだわりその③</h4>
+                <p class="text-center">新築のお家はもちろんのこと、リフォーム後のお家のメンテナンスやお困り事にも誠意をもって対応します。</p>
             </div>
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mt100">
             <div>
-                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img8.png" alt="吹き出し">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img8.png" alt="吹き出し">
             </div>
         </div>
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 my150">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 d-none d-md-block my150">
             <div class="text-center">
-                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img9.png" alt="全館空調" width="811px">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img9.png" alt="全館空調" width="811px">
             </div>
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="text-center d-none d-md-block">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/zkuchou_2.jpg" alt="Z空調">
+            </div>
+            <div class="text-center d-block d-md-none">
+                <img class="z-img" src="<?php echo get_template_directory_uri(); ?>/images/subsidy/subsidy-img15.png" alt="Z空調" width="207px">
+            </div>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 mb100">
             <div class="text-center">
-                <img class="img-figure" src="<?php echo get_template_directory_uri(); ?>/images/zkuchou_2.jpg" alt="Z空調">
+                <a class="subsidy-btn" href="#">詳しくはこちら</a>
             </div>
         </div>
     </div>
