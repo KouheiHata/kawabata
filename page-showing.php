44dba92234855a95494902f544b995e7290e3eed
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2021-02-01 10:59:47 +0900

    保守メンテ
    画像差し替え

--- page-showing.php
+++ page-showing.php
@@ -7,7 +7,7 @@ Template Name: 内覧会予約
 
 <?php get_header(); ?>
 
-<img src="http://www.kawabata-koumuten.com/wp-content/uploads/2021/01/152cca98f51b219d3390b60131288970.jpg" style="display:block; margin: 0 auto; width: 90vw;">
+<img src="http://www.kawabata-koumuten.com/wp-content/uploads/2021/01/11573d0f38a7221cbca520b69426d177.png" style="display:block; margin: 0 auto; width: 90vw;">
 
 <div class="container news-top">
     <div class="row">
@@ -16,6 +16,14 @@ Template Name: 内覧会予約
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
 <?php echo do_shortcode('[contact-form-7 id="1176" title="内覧会予約"]'); ?>
+        </div>
+   </div>
+   <div class="row">
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 pt-3">
+        <a href="http://www.kawabata-koumuten.com/wp-content/uploads/2021/01/5c9b5e2e41418cf650a3d8169278e6a7.jpg"><img src="http://www.kawabata-koumuten.com/wp-content/uploads/2021/01/5c9b5e2e41418cf650a3d8169278e6a7.jpg" alt="川端工務店_Z空調の家" class="aligncenter size-full wp-image-1221" /></a>
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 pt-3">
+        <a href="http://www.kawabata-koumuten.com/wp-content/uploads/2021/01/IMG_5042.jpg"><img src="http://www.kawabata-koumuten.com/wp-content/uploads/2021/01/IMG_5042.jpg" alt="Z空調202101_2" class="aligncenter size-full wp-image-1367" /></a>
         </div>
    </div>
 </div>
