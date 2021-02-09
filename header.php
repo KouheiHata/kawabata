0925689178c7f29986997b2dadfa8fe0caff1b38
shi-hatta <s_hatta@standtogether-fukushi.com>
2021-02-09 15:01:21 +0900

    ヘッダー調整/PCサイズ
    何故か文字が下にズレる問題が発生

--- header.php
+++ header.php
@@ -89,10 +89,10 @@
                     <img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="株式会社　川端工務店">
                 </a>
             </div>
-            <div class="col-lg-6 col-md-5 col-sm-12 col-xs-12 logo d-none d-md-block">
-                <p class="font-weight-bold text-md-right h5 mt-2">
+            <div class="col-lg-6 col-md-5 col-sm-12 col-xs-12 logo d-none d-md-block text-right">
+                <p class="d-none d-sm-inline-block font-weight-bold text-md-right h5 mt-3 pr-2"><a href="<?php echo esc_url(home_url('/contact')); ?>">お問い合わせはこちら</a></p>
+                <p class="d-inline-block font-weight-bold text-md-right h5 mt-3">
                 <a href="tel:0778-22-6577"><i class="fas fa-phone-square"></i>&nbsp;0778-22-6577</a></p>
-            <p class="d-none d-sm-block font-weight-bold text-md-right h5 mt-2"><a href="<?php echo esc_url(home_url('/contact')); ?>">お問い合わせはこちら</a></p>
             </div>
         </div>
     </div>
@@ -104,36 +104,36 @@
     <div class="sns-menu-contents">
         <ul>
             <li>
-                <div class="pt-2 pr-3 sidebar-width">
+                <div class="pt-5 pr-3 sidebar-width mx-auto sns-sm">
                    <h4 class="text-left inst"><a href="https://www.instagram.com/kawabatakoumuten226577/"><i class="fab fa-instagram fa-fw"></i>instagram</a></h4>
                     <?php echo do_shortcode('[instagram-feed width=115]'); ?>
                </div>
             </li>
             <li>
-                <div class="pt-2 sidebar-width">
+                <div class="pt-5 sidebar-width mx-auto sns-sm">
                     <h4 class="text-left inst pb-3"><a href="https://www.facebook.com/kawabata.koumuten/"><i class="fab fa-facebook fa-fw"></i>facebook</a></h4>
                     <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fkawabata.koumuten%2F&tabs=timeline&width=285&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="285" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
                 </div>
             </li>
             <li>
-                <div class="pt-2 sidebar-width">
+                <div class="pt-5 sidebar-width mx-auto sns-sm">
                     <h4 class="text-left inst"><a href="https://www.youtube.com/channel/UCE4kChI3_d2FFjFHA3Hj8Iw"><i class="fab fa-youtube fa-fw"></i>YouTube</a></h4><br />
                     <iframe width="285" height="157" src="https://www.youtube.com/embed/videoseries?list=PLzNCnTkLCFG2jdO8TXtuxAbTeohvRD5BS" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>]
                 </div>
             </li>
             <li>
-                <div class="pt-2 sidebar-width">
+                <div class="pt-5 sidebar-width mx-auto sns-sm">
                     <h4 class="text-left inst pb-3">
                         <i class="fab fa-pinterest"></i>
                         <a href="https://www.pinterest.jp/kawabatakoumuten226577/">Pinterest</a>
                     </h4>
-                    <div style="width: 25vw!important">
+                    <div>
                     <a data-pin-do="embedUser" data-pin-lang="ja" data-pin-board-width="25vw" data-pin-scale-height="240" data-pin-scale-width="7vw" href="https://www.pinterest.jp/kawabatakoumuten226577/"></a>
                     </div>
                 </div>
             </li>
             <li>
-                <div class="pt-2 sidebar-width">
+                <div class="pt-5 sidebar-width mx-auto sns-sm">
                     <h4 class="text-left inst pb-3">
                         <img src="<?php echo get_template_directory_uri(); ?>/images/icon_line.png" width="30px">
                         LINE@
