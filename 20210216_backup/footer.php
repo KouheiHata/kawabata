6b852fdc46808553a64e9bb77f5614e8d2706e67
shi-hatta <s_hatta@standtogether-fukushi.com>
2021-02-17 14:21:01 +0900

    バックアップを取りつつヘッダーを新規作成

--- 20210216_backup/footer.php
+++ 20210216_backup/footer.php
@@ -107,6 +107,16 @@ $(function($){
 </script>
 <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
 <script>
+const ham = document.getElementById('ham');
+const menu_wrapper =
+document.getElementById('menu_wrapper');
+ham.addEventListener('click', function() {
+    ham.classList.toggle('clicked');
+    menu_wrapper.classList.toggle('clicked');
+});
+</script>
+
+<script>
     $(function(){
         $(".sns-menu-contents").hide();
         $(".menubtn").click(function(){
