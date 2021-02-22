537d2a46b062d972569bbb5144610ec9c33de8c4
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2021-02-22 13:13:04 +0900

    Revert "レスポンシブ対応（Youtube動画）"
    This reverts commit f48c22cdd8505d54e895a2f8f45124b20d2817c3.

--- front-page.php
+++ front-page.php
@@ -237,7 +237,7 @@ Template Name: 川端工務店フロントページ
                 <!-- Youtube -->
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 pt-5">
                     <h4 class="text-left inst"><a href="https://www.youtube.com/channel/UCE4kChI3_d2FFjFHA3Hj8Iw"><i class="fab fa-youtube fa-fw"></i>YouTube</a></h4><br />
-                    <iframe class="youtube-video" src="https://www.youtube.com/embed/videoseries?list=PLzNCnTkLCFG2jdO8TXtuxAbTeohvRD5BS" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <iframe width="285" height="157" src="https://www.youtube.com/embed/videoseries?list=PLzNCnTkLCFG2jdO8TXtuxAbTeohvRD5BS" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div><!-- Youtube end -->
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 pt-5">
                     <h4 class="text-left inst pb-3">
