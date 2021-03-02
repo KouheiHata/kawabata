9eb041b73ff29e0ca6142a49c4d2ed6134f489c6
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2021-03-02 15:13:46 +0900

    トップページレスポンシブ対応のアニメーション
    浮き上がるアニメーション

--- footer.php
+++ footer.php
@@ -210,6 +210,23 @@ function arrow() {
 	$('img[usemap]').rwdImageMaps();
 </script>
 
+<!-- rwdImageMaps -->
+<script>
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 100){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
+    });
+});
+</script>
+
 <?php wp_footer(); ?>
 </body>
 </html>
