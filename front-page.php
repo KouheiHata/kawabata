84693ea502488c9209c336a336dc8d239dbbd08b
yamaguchi371 <telework-takefu6@outlook.jp>
2021-06-01 10:24:40 +0900

    popイメージ差し替え

--- front-page.php
+++ front-page.php
@@ -23,9 +23,9 @@ Template Name: 川端工務店フロントページ
         <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-12"><!-- （メイン部分） start-->
                 <div class="row">
                     <div class="pop-area">
-                        <a href="<?php echo home_url('/hojyokin/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/pop1.svg" class="pop1 fadein" alt="補助金に関する情報はこちら"></a>
-                        <a href="<?php echo home_url('/zkuutyou/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/pop2.svg" class="pop2 fadein" alt="Z空調に関する情報はこちら"></a>
-                        <a href="<?php echo home_url('/longlife/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/pop3.svg" class="pop3 fadein" alt="長期優良住宅に関する情報はこちら"></a>
+                        <a href="<?php echo home_url('/hojyokin/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/pop1.png" class="pop1 fadein" alt="補助金に関する情報はこちら"></a>
+                        <a href="<?php echo home_url('/zkuutyou/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/pop2.png" class="pop2 fadein" alt="Z空調に関する情報はこちら"></a>
+                        <a href="<?php echo home_url('/longlife/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/pop3.png" class="pop3 fadein" alt="長期優良住宅に関する情報はこちら"></a>
                     </div><!-- pop-area -->
                 </div>
                 <div class="row"><!-- row（メイン部分内） -->
@@ -251,7 +251,7 @@ Template Name: 川端工務店フロントページ
                 <!-- Youtube -->
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 pt-5">
                     <h4 class="text-left inst"><a href="https://www.youtube.com/channel/UCE4kChI3_d2FFjFHA3Hj8Iw"><i class="fab fa-youtube fa-fw"></i>YouTube</a></h4><br />
-                    <iframe class="youtube-video" src="https://www.youtube.com/embed/zn9NNMu4U94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe class="youtube-video" src="https://www.youtube.com/embed/_Jp6zwJ9mwk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div><!-- Youtube end -->
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 pt-5">
                     <h4 class="text-left inst pb-3">
