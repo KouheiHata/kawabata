6d8f038f5f023d2ebe3bc3dce6a8277c0711368f
shi-hatta <s_hatta@standtogether-fukushi.com>
2021-07-21 13:01:26 +0900

    スタッフ紹介修正/スマホサイズ調整

--- page-staffpresident.php
+++ page-staffpresident.php
@@ -30,66 +30,73 @@ Template Name: 川端工務店スタッフ紹介個別ページ
             </div>
             <div class="staffpage-certification">
                 <a class="certification01" style="cursor:pointer;">一級建築士
-                    <div class="stafftri-01"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_down.jpg"></div>
-                    <div class="stafftri-02"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_up.jpg"></div>
+                    <div class="stafftri stafftri-01"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_down.jpg"></div>
+                    <div class="stafftri stafftri-02 hide"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_up.jpg"></div>
                 </a>
             </div>
             <div class="certification01-img hide">
-                <div><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/09.png" alt="一級建築士"></div>
+                <div class="d-none d-md-block"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/09.png" alt="一級建築士"></div>
+                <div class="d-block d-md-none"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/03sm.png" alt="一級建築士"></div>
             </div>
             <div class="staffpage-certification">
                 <a class="certification02" style="cursor:pointer;">一級建築施工管理技士
-                    <span class="stafftri-03"><i class="fas fa-angle-down fa-lg fa-fw"></i></span>
-                    <span class="stafftri-04 hide"><i class="fas fa-angle-up fa-lg fa-fw"></i></span>
+                    <div class="stafftri stafftri-03"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_down.jpg"></div>
+                    <div class="stafftri stafftri-04 hide"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_up.jpg"></div>
                 </a>
             </div>
             <div class="certification02-img hide">
-                <div><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/08.png" alt="一級建築施工管理技士"></div>
+                <div class="d-none d-md-block"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/08.png" alt="一級建築施工管理技士"></div>
+                <div class="d-block d-md-none"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/02sm.png" alt="一級建築施工管理技士"></div>
             </div>
             <div class="staffpage-certification">
                 <a class="certification03" style="cursor:pointer;">一級土木施工管理技士
-                    <span class="stafftri-05"><i class="fas fa-angle-down fa-lg fa-fw"></i></span>
-                    <span class="stafftri-06 hide"><i class="fas fa-angle-up fa-lg fa-fw"></i></span>
+                    <div class="stafftri stafftri-05"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_down.jpg"></div>
+                    <div class="stafftri stafftri-06 hide"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_up.jpg"></div>
                 </a>
             </div>
             <div class="certification03-img hide">
-                <div><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/07.png" alt="一級土木施工管理技士"></div>
+                <div class="d-none d-md-block"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/07.png" alt="一級土木施工管理技士"></div>
+                <div class="d-block d-md-none"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/01sm.png" alt="一級土木施工管理技士"></div>
             </div>
             <div class="staffpage-certification">
                 <a class="certification04" style="cursor:pointer;">宅地建物取引士
-                    <span class="stafftri-07"><i class="fas fa-angle-down fa-lg fa-fw"></i></span>
-                    <span class="stafftri-08 hide"><i class="fas fa-angle-up fa-lg fa-fw"></i></span>
+                    <div class="stafftri stafftri-07"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_down.jpg"></div>
+                    <div class="stafftri stafftri-08 hide"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_up.jpg"></div>
                 </a>
             </div>
             <div class="certification04-img hide">
-                <div><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/06.png" alt="宅地建物取引士"></div>
+                <div class="d-none d-md-block"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/06.png" alt="宅地建物取引士"></div>
+                <div class="d-block d-md-none"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/07sm.png" alt="宅地建物取引士"></div>
             </div>
             <div class="staffpage-certification">
                 <a class="certification05" style="cursor:pointer;">住宅ローンアドバイザー
-                    <span class="stafftri-09"><i class="fas fa-angle-down fa-lg fa-fw"></i></span>
-                    <span class="stafftri-10 hide"><i class="fas fa-angle-up fa-lg fa-fw"></i></span>
+                    <div class="stafftri stafftri-09"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_down.jpg"></div>
+                    <div class="stafftri stafftri-10 hide"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_up.jpg"></div>
                 </a>
             </div>
             <div class="certification05-img hide">
-                <div><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/05.png" alt="住宅ローンアドバイザー"></div>
+                <div class="d-none d-md-block"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/05.png" alt="住宅ローンアドバイザー"></div>
+                <div class="d-block d-md-none"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/05sm.png" alt="住宅ローンアドバイザー"></div>
             </div>
             <div class="staffpage-certification">
                 <a class="certification06" style="cursor:pointer;">インテリアコーディネーター
-                    <span class="stafftri-11"><i class="fas fa-angle-down fa-lg fa-fw"></i></span>
-                    <span class="stafftri-12 hide"><i class="fas fa-angle-up fa-lg fa-fw"></i></span>
+                    <div class="stafftri stafftri-11"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_down.jpg"></div>
+                    <div class="stafftri stafftri-12 hide"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_up.jpg"></div>
                 </a>
             </div>
             <div class="certification06-img hide">
-                <div><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/04.png" alt="インテリアコーディネーター"></div>
+                <div class="d-none d-md-block"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/04.png" alt="インテリアコーディネーター"></div>
+                <div class="d-block d-md-none"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/04sm.png" alt="インテリアコーディネーター"></div>
             </div>
             <div class="staffpage-certification">
                 <a class="certification07" style="cursor:pointer;">測量士
-                    <span class="stafftri-13"><i class="fas fa-angle-down fa-lg fa-fw"></i></span>
-                    <span class="stafftri-14 hide"><i class="fas fa-angle-up fa-lg fa-fw"></i></span>
+                    <div class="stafftri stafftri-13"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_down.jpg"></div>
+                    <div class="stafftri stafftri-14 hide"><img src="<?php echo get_template_directory_uri(); ?>/images/angle_up.jpg"></div>
                 </a>
             </div>
             <div class="certification07-img hide">
-                <div><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/03.png" alt="測量士"></div>
+                <div class="d-none d-md-block"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/03.png" alt="測量士"></div>
+                <div class="d-block d-md-none"><img src="<?php echo get_template_directory_uri(); ?>/images/qualification/06sm.png" alt="測量士"></div>
             </div>
         </div>
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -97,9 +104,9 @@ Template Name: 川端工務店スタッフ紹介個別ページ
                 <p class="text-center">他のスタッフを見る</p>
             </div>
         </div>
-        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 staff-card text-center">
-            <div>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/staff_top2.png" width="250px" alt="スタッフ紹介画像">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6 staff-card text-center">
+            <div class="staff-card-box">
+                <img class="staff-img" src="<?php echo get_template_directory_uri(); ?>/images/staff_top2.png" width="250px" alt="スタッフ紹介画像">
                 <div class="staff-cardbox">
                     <div>
                         <p class="staff-position">代表取締役社長</p>
@@ -111,23 +118,9 @@ Template Name: 川端工務店スタッフ紹介個別ページ
                 </div>
             </div>
         </div>
-        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 staff-card text-center">
-            <div>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/staff_top2.png" width="250px" alt="スタッフ紹介画像">
-                <div class="staff-cardbox">
-                    <div>
-                        <p class="staff-position">代表取締役社長</p>
-                    </div>
-                    <div class="staff-name">
-                        <p class="sub-text">Takeshi&ensp;Kawabata</p>
-                        <a href="#"><p class="name">川端&ensp;武志</p></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-xs-12 staff-card text-center">
-            <div>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/staff_top2.png" width="250px" alt="スタッフ紹介画像">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-6 staff-card text-center">
+            <div class="staff-card-box">
+                <img class="staff-img" src="<?php echo get_template_directory_uri(); ?>/images/staff_top2.png" width="250px" alt="スタッフ紹介画像">
                 <div class="staff-cardbox">
                     <div>
                         <p class="staff-position">代表取締役社長</p>
